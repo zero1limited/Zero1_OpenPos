9a2f2a125e09bffe54c896a745e7324fb314b0e3
Arron Moss <arron.moss@zero1.co.uk>
2023-12-24 14:04:37 +0000

    Update QuoteItemQuantityBypass.php

--- Plugin/QuoteItemQuantityBypass.php
+++ Plugin/QuoteItemQuantityBypass.php
@@ -1,10 +1,10 @@
 <?php
 
-namespace Zero1\Pos\Plugin;
+namespace Zero1\OpenPos\Plugin;
 
 use Magento\CatalogInventory\Model\Quote\Item\QuantityValidator;
 use Magento\Framework\Event\Observer;
-use Zero1\Pos\Helper\Data as PosHelper;
+use Zero1\OpenPos\Helper\Data as PosHelper;
 
 class QuoteItemQuantityBypass
 {
