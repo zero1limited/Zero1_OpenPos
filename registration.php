c8ba5f0271c2a228a3671fa6ec8ce77a920b391a
Arron Moss <arron.moss@zero1.co.uk>
2023-12-24 14:22:08 +0000

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
     \Magento\Framework\Component\ComponentRegistrar::register(
         \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Zero1_Pos',
+        'Zero1_OpenPos',
         __DIR__
     );
